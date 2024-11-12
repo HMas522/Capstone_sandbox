ab941ce52b29db01656a6a2fde0a850f97d6ed8e
Hayley Massey <HMassey12@outlook.com>
2024-11-11 20:22:30 -0600

    added base display board

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+from shiny.express import input, render, ui
+from shinyswatch import theme
+
+ui.page_opts(title="Hmass Dashboard", fillable=True)
+
+
 from pathlib import Path
 
 import pandas
@@ -5,10 +11,35 @@ import pandas
 from shiny import reactive
 from shiny.express import render, ui
 
+with ui.sidebar(open="desktop"):
+    ui.h2("Sidebar")
+    ui.tags.hr()
+    ui.h3("Interaction")
+    ui.input_text("name_input", "Enter your name", placeholder="Your Name")
+    ui.input_text("language_input", "Enter your favorite language(s)", placeholder="Favorite Programming Language(s)")
+    ui.tags.hr()
+    ui.h3("Links")
+    ui.a("PyShiny", href="https://shiny.posit.co/py/", target="_blank")
+    ui.a("Examples", href="https://shinylive.io/py/examples/", target="_blank")
+    ui.a("Themes", href="https://posit-dev.github.io/py-shinyswatch/", target="_blank")
+    ui.a("Deploy", href="https://docs.posit.co/shinyapps.io/getting-started.html#working-with-shiny-for-python", target="_blank")
+    ui.a("GitHub", href="https://github.com/denisecase", target="_blank")
+    ui.a("GitHub Repository", href="https://github.com/denisecase/cintel-02-app", target="_blank")
+    ui.a("Shinylive.io App", href="https://denisecase.shinyapps.io/cintel-02-app/", target="_blank")
+   
+with ui.layout_columns(fill=False):       
+    ui.h2("Main Area with Reactive Output")
+ 
+  
+@render.text
+def welcome_output():
+    user = input.name_input();
+    welcome_string = f'Greetings {user}!';
+    return welcome_string
 
 @reactive.calc
 def dat():
-    infile = Path(__file__).parent / "C:\Users\Hayley\Capstone_sandbox\English_Premier_League_standings.csv"
+    infile = Path(__file__).parent / "English_Premier_League_standings.csv"
     return pandas.read_csv(infile)
 
 
