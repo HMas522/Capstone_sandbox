754c0e04a0931f72a44beca2828236ca9dd6b79d
Hayley Massey <HMassey12@outlook.com>
2024-11-12 18:58:38 -0600

    launching shiny with table & dataframe exmp

--- app.py
+++ app.py
@@ -1,39 +1,53 @@
+"""
+Purpose: Use Python to create a continuous intelligence and 
+interactive analytics dashboard using Shiny for Python with 
+interactive charts from HoloViews Bokeh and Plotly Express.
 
-from shiny.express import input, render, ui
-from shinyswatch import theme
+Each Shiny app has two parts: 
 
-ui.page_opts(title="Hmass Dashboard", fillable=True)
-# theme.minty()
-# theme.cerulean()
+- a user interface app_ui object (similar to the HTML in a web page) 
+- a server function that provides the logic for the app (similar to JS in a web page).
 
-with ui.sidebar(open="desktop"):
-    ui.h2("Sidebar")
-    ui.tags.hr()
-    ui.h3("Interaction")
-    ui.input_text("name_input", "Enter your name", placeholder="Your Name")
-    ui.input_text("language_input", "Enter your favorite language(s)", placeholder="Favorite Programming Language(s)")
-    ui.tags.hr()
-    ui.h3("Links")
-    ui.a("PyShiny", href="https://shiny.posit.co/py/", target="_blank")
-    ui.a("Examples", href="https://shinylive.io/py/examples/", target="_blank")
-    ui.a("Themes", href="https://posit-dev.github.io/py-shinyswatch/", target="_blank")
-    ui.a("Deploy", href="https://docs.posit.co/shinyapps.io/getting-started.html#working-with-shiny-for-python", target="_blank")
-   
-   
-with ui.layout_columns(fill=False):       
-    ui.h2("Main Area with Reactive Output")
- 
-  
-@render.text
-def welcome_output():
-    user = input.name_input()
-    welcome_string = f'Greetings {user}!'
-    return welcome_string
+"""
+from shiny import App, ui
+import shinyswatch
+
+from mtcars_server import get_mtcars_server_functions
+from mtcars_ui_inputs import get_mtcars_inputs
+from mtcars_ui_outputs import get_mtcars_outputs
 
 
-@render.text
-def insights_output():
-    answer = input.language_input()
-    count = len(answer)
-    language_string = f'You like {answer}. That takes {count} characters'
-    return language_string
+from util_logger import setup_logger
+
+logger, logname = setup_logger(__name__)
+
+app_ui = ui.page_navbar(
+    shinyswatch.theme.journal(),
+    
+    ui.nav(
+        "MT_Cars",
+        ui.layout_sidebar(
+            get_mtcars_inputs(),
+            get_mtcars_outputs(),
+        ),
+    ),
+    
+    ui.nav(ui.a("About", href="https://github.com/HMas522")),
+    ui.nav(ui.a("GitHub", href="https://github.com/HMas522/cintel-04-reactive")),
+    ui.nav(ui.a("App", href="https://HMas522.shinyapps.io/cintel-04-reactive/")),
+    ui.nav(ui.a("Examples", href="https://shinylive.io/py/examples/")),
+    ui.nav(ui.a("Widgets", href="https://shiny.rstudio.com/py/docs/ipywidgets.html")),
+    title=ui.h1("Hmas522 Dashboard"),
+)
+
+
+def server(input, output, session):
+    """Define functions to create UI outputs."""
+
+    logger.info("Starting server...")
+    
+    get_mtcars_server_functions(input, output, session)
+    
+
+# app = App(app_ui, server, debug=True)
+app = App(app_ui, server)
