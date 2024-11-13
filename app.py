62d84efd91c5efc7e81b574da870e11a9776da0b
Hayley Massey <HMassey12@outlook.com>
2024-11-12 20:22:06 -0600

    deploy app?

--- app.py
+++ app.py
@@ -1,44 +1,39 @@
-from pathlib import Path
-import pandas as pd
-from shiny import App, ui, render, reactive
-import shinyswatch  # For themes
 
-# Define the reactive data fetching function
-@reactive.calc
-def dat() -> pd.DataFrame:
-    infile = Path(__file__).parent / "English_Premier_League_standings.csv"
-    return pd.read_csv(infile)
+from shiny.express import input, render, ui
+from shinyswatch import theme
 
-# Define the UI with shinyswatch theme
-app_ui = ui.page_navbar(
-    shinyswatch.theme.journal(),  # Apply the journal theme from shinyswatch
-    ui.navset_card_underline(  # Set up a tabbed navigation layout
-        ui.nav_panel("Data frame", 
-            ui.output_data_frame("frame")  # Output for the data frame tab
-        ),
-        ui.nav_panel("Table", 
-            ui.output_table("table")  # Output for the table tab
-        )
-    )
-)
+ui.page_opts(title="Hmass Dashboard", fillable=True)
+# theme.minty()
+# theme.cerulean()
 
-# Define the server function
-def server(input, output, session):
-    # Render the data frame for the "Data frame" tab
-    @output()
-    @render.data_frame
-    def frame() -> pd.DataFrame:
-        return dat()
-    
-    # Render the table for the "Table" tab
-    @output()
-    @render.table
-    def table() -> pd.DataFrame:
-        return dat()
+with ui.sidebar(open="desktop"):
+    ui.h2("Sidebar")
+    ui.tags.hr()
+    ui.h3("Interaction")
+    ui.input_text("name_input", "Enter your name", placeholder="Your Name")
+    ui.input_text("language_input", "Enter your favorite language(s)", placeholder="Favorite Programming Language(s)")
+    ui.tags.hr()
+    ui.h3("Links")
+    ui.a("PyShiny", href="https://shiny.posit.co/py/", target="_blank")
+    ui.a("Examples", href="https://shinylive.io/py/examples/", target="_blank")
+    ui.a("Themes", href="https://posit-dev.github.io/py-shinyswatch/", target="_blank")
+    ui.a("Deploy", href="https://docs.posit.co/shinyapps.io/getting-started.html#working-with-shiny-for-python", target="_blank")
+   
+   
+with ui.layout_columns(fill=False):       
+    ui.h2("Main Area with Reactive Output")
+ 
+  
+@render.text
+def welcome_output():
+    user = input.name_input();
+    welcome_string = f'Greetings {user}!';
+    return welcome_string
 
-# Create the app
-app = App(app_ui, server)
 
-# Run the app
-if __name__ == "__main__":
-    app.run()
+@render.text
+def insights_output():
+    answer = input.language_input()
+    count = len(answer)
+    language_string = f'You like {answer}. That takes {count} characters'
+    return language_string
